76b9911d8f2375f9bf21c5e87c77924f12d379de
Argon <vongotha0@gmail.com>
2025-10-16 23:22:30 +0100

    Changing background and Globe Material

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import { drawThreeGeo } from "./src/threeGeoJSON.js";
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
-scene.fog = new THREE.FogExp2(0x000000, 0.3);
+//scene.fog = new THREE.FogExp2(0x000000, 0.3);
+const laitColor = 0xF0F0F0;
+scene.fog = new THREE.FogExp2(laitColor, 0.05);
 const camera = new THREE.PerspectiveCamera(75, w / h, 1, 100);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -21,13 +23,24 @@ controls.maxDistance = 4.9;
 const geometry = new THREE.SphereGeometry(2);
 geometry.color = 0x000000;
 const lineMat = new THREE.LineBasicMaterial({ 
-  color: 0xffffff,
+  color: 0x444444,
   transparent: true,
-  opacity: 0.1, 
+  opacity: 0.15,
 });
 const edges = new THREE.EdgesGeometry(geometry, 1);
 const line = new THREE.LineSegments(edges, lineMat);
 scene.add(line);
+
+scene.background = new THREE.Color(laitColor);
+
+const softWhite = 0xEEEEEE;
+
+const ambientLight = new THREE.AmbientLight(softWhite, 0.8);
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(softWhite, 0.3); 
+directionalLight.position.set(5, 5, 5); // Position de la source
+scene.add(directionalLight);
 
 const stars = getStarfield({ numStars: 1000, fog: false });
 scene.add(stars);
@@ -43,7 +56,9 @@ fetch('./geojson/ne_110m_coastline.json')
       json: data,
       radius: 2,
       materialOptions: {
-        color: 0x80FF80,
+        color: 0x6666AA,
+        transparent: true,
+        opacity: 0.5
       },
     });
     scene.add(countries);
